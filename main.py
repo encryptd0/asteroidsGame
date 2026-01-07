c51a2e27f53596dba28ada44ef76aec535f7b1ee
Riekus Grobler <riekusgroblerps4@gmail.com>
2026-01-07 15:15:13 +0200

    changed the time to delta time to make the gams behavior independent of fps

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    dt = 0
     
     while True:    
         for event in pygame.event.get():
@@ -19,7 +20,7 @@ def main():
         pygame.display.flip()
      
      
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
