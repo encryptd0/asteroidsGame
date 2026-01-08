dea4b384cdecfe68a558a6824b3629f11c50e627
Riekus Grobler <riekusgroblerps4@gmail.com>
2026-01-08 13:49:29 +0200

    fixed bugs. added groupes to make the game scalable when introducing new entities

--- main.py
+++ main.py
@@ -5,25 +5,37 @@ from player import Player
 
 
 def main():
+    #Groups
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()  
+    #Player class added to groups updatable and drawable 
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
     
     # Game loop 
+    
     while True:   
         dt = clock.tick(60) / 1000
-         
+               
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         
         log_state()
         
         screen.fill("black")
-        player.draw(screen)
+        #Loop over and render all drawable objects 
+        
+        for obj in drawable: 
+            obj.draw(screen)
+            
         pygame.display.flip()
      
     
