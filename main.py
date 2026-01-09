417bf3af7cd689bae54e35ff507500aba9fa2d01
Riekus Grobler <riekusgroblerps4@gmail.com>
2026-01-09 10:48:36 +0200

    added asteroids and asteroid field spawn

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     #Groups
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()  
+    asteroids = pygame.sprite.Group()
+    
     #Player class added to groups updatable and drawable 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     
+    #Objects
     player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
-    
+    asteroid_field = AsteroidField()
     # Game loop 
-    
     while True:   
         dt = clock.tick(60) / 1000
                
